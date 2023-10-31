2c84454ce9f2af5844518f7b4fa0587e1aa8e59d
burgokmen <burgokmen@gmail.com>
2023-10-31 16:16:59 +0300

    Role entity created

---
+++ src/main/java/com/brutech/secure_rest_api/entity/Role.java
@@ -0,0 +1,19 @@
+package com.brutech.secure_rest_api.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "role", schema = "fsweb")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name="authority")
+    private String authority;
+}
