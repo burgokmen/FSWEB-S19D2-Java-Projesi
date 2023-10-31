06ed8810c961110b3baa04f85aa29138d7d57f0e
burgokmen <burgokmen@gmail.com>
2023-10-31 22:30:57 +0300

    Member entity

--- src/main/java/com/brutech/secure_rest_api/entity/Member.java
+++ src/main/java/com/brutech/secure_rest_api/entity/Member.java
@@ -7,7 +7,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.Set;
+import java.util.List;
+
 
 @Data
 @NoArgsConstructor
@@ -27,41 +28,41 @@ public class Member implements UserDetails {
     @JoinTable(name = "member_role", schema = "fsweb",
             joinColumns = @JoinColumn(name = "member_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> authorities;
+    private List<Role> authorities;
 
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
